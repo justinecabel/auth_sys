6369fa71f483d0d7afb114bf9ebfaa8268bd4d7f
Justine Cabel <justinecabel2@gmail.com>
2025-05-03 22:38:03 +0800

    fix v1

--- tools/scripts/hasher.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("Content-Type: application/json");
-$data = json_decode(file_get_contents("php://input"), true);
-$text = $data['input'] ?? '';
-
-echo json_encode(["result" => password_hash($text, PASSWORD_DEFAULT)]);
